277277f27e20d7150ebdf6f7ed36751d853de721
Vinicius Alexander Moreira Nascimento <vinicius.malex2002@gmail.com>
2025-07-09 09:42:11 -0300

    feat: create the database and front end aplication

---
+++ web/src/app.tsx
@@ -0,0 +1,21 @@
+import "./index.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
+const queryClient = new QueryClient();
+export function App() {
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<CreateRoom />} index />
+            <Route path="/room/:roomId" element={<Room />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </div>
+  );
+}
