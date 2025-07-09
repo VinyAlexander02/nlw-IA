ed6ca9367c3a530e4d61116e439209bba3e69402
Vinicius Alexander Moreira Nascimento <vinicius.malex2002@gmail.com>
2025-07-09 20:23:06 -0300

    feat: add create quetion, create room and get room questions

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ import fastifyCors from "@fastify/cors";
 import { env } from "./env.ts";
 import { sql } from "./db/connection.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { createRoomRoute } from "./http/routes/create-room.ts";
+import { getRoomQuestions } from "./http/routes/get-room-questions.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -23,6 +25,8 @@ app.get("/health", () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomQuestions);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Port: ${process.env.PORT}`);
